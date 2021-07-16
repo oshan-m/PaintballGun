4a188ca0fbfb606342cbfe681f3d9071edcf854b
oshan.m <oshan.m@yahoo.com>
2021-07-16 19:37:21 +1000

    Add project files.

---
+++ PaintballGun/PaintballGun.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaintballGun
+{
+    class PaintballGun
+    {
+        public int MagazineSize { get; set; } = 16;
+
+        private int balls = 0;
+        public int BallsLoaded { get; private set; }
+
+
+        public PaintballGun(int balls, int magazineSize, bool loaded) 
+        {
+            this.balls = balls;
+            MagazineSize = magazineSize;
+            if (!loaded) Reload();
+        }
+        public bool IsEmpty() { return BallsLoaded == 0; }
+
+        public int Balls
+        {
+            get { return balls; }
+            set
+            {
+                if (value > 0)
+                    balls = value;
+                Reload();
+            }
+        }
+
+        public void Reload()
+        {
+            if (balls > MagazineSize)
+                BallsLoaded = MagazineSize;
+            else
+                BallsLoaded = balls;
+        }
+
+        public bool Shoot()
+        {
+            if (BallsLoaded == 0) return false;
+            BallsLoaded--;
+            balls--;
+            return true;
+        }
+    }
+}
---
+++ PaintballGun/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PaintballGun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaintballGun gun = new PaintballGun(20,16,false);
+            while (true)
+            {
+                Console.WriteLine($"{gun.Balls} balls, {gun.BallsLoaded} loaded");
+                if (gun.IsEmpty()) Console.WriteLine("WARNING: You’re out of ammo");
+                Console.WriteLine("Space to shoot, r to reload, + to add ammo, q to quit");
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == ' ') Console.WriteLine($"Shooting returned {gun.Shoot()}");
+                else if (key == 'r') gun.Reload();
+                else if (key == '+') gun.Balls  += gun.MagazineSize;
+                else if (key == 'q') return;
+            }
+
+        }
+    }
+}
